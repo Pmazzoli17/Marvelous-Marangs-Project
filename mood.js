c28fc0635c664ddb875a78c8357ea5043864b1de
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-01-18 13:07:10 -0500

    linked javascript and email input to store in firebase

--- mood.js
+++ mood.js
@@ -1,32 +1,47 @@
-// Initialize Firebase
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB6Ci3DroFdkgx-eOP-95QmDoGbqIH1-34",
+    authDomain: "marvelous-marangs-project.firebaseapp.com",
+    databaseURL: "https://marvelous-marangs-project.firebaseio.com",
+    storageBucket: "marvelous-marangs-project.appspot.com",
+    messagingSenderId: "897117282396"
+    };
 
+    firebase.initializeApp(config);
 
+  // Create a variable to reference the database
+  var database = firebase.database();
 
-// Email input field 
+  // Email input field 
+    // Click Button changes what is stored in firebase
+    $("#submit").on("click", function() {
 
+      // Get inputs
+      email = $("#userEmail").val().trim();
 
-// When users click "submit"
-    $(document.body).on("click", "#submit", function(event) {
+      // Change what is saved in firebase
+      database.ref().set({
+        email: email,
+      });
 
-    	console.log("submit was clicked");
-
-      event.preventDefault();
-
-      // Grab the user input
-      var email = $("#userEmail").val().trim();
-
-      console.log(email);
-
-      // Clear absolutely everything stored in localStorage using localStorage.clear()
-      localStorage.clear();
-
-      // Store the username into localStorage using "localStorage.setItem"
-      localStorage.setItem("email", email);
-
-      // This line prevents the page from refreshing when a user hits "enter".
-      // return false;
+      // Prevent the page from refreshing
+      return false;
     });
 
+    // Firebase is always watching for changes to the data.
+    // When changes occurs it will print them to console and html
+    database.ref().on("value", function(snapshot) {
+
+      // Print the initial data to the console.
+      console.log(snapshot.val());
+
+      // Log the value of the various properties
+      console.log(snapshot.val().email);
+
+      // If any errors are experienced, log them to console.
+    }, function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
 
 // Mood Buttons
 
